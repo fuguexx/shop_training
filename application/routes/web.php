0354a2c1a80c5bf5b297c8eb8871c18446b0cdf8
神澤尚 <kamizawasho@MacBook-Air.lan>
2020-02-23 16:12:18 +0900

    front auth functions start

--- application/routes/web.php
+++ application/routes/web.php
@@ -10,25 +10,43 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::namespace('User')->group(function(){
+    Route::get('products', 'ProductController@index');
+    Route::get('products/{product}', 'ProductController@show');
+});
 
-/* admin ログインページへのリダイレクト */
+Route::namespace('User')->middleware(['auth:user'])->group(function(){
+    Route::get('home', 'HomeController@index')->name('home');
+    Route::get('products/{product}/product_reviews/create', 'ProductReviewController@create');
+    Route::post('products/{product}/product_reviews', 'ProductReviewController@store');
+    Route::get('products/{product}/product_reviews/{product_review}/edit', 'ProductReviewController@edit');
+    Route::post('products/{product}/product_reviews/{product_review}', 'ProductReviewController@update');
+});
+
 Route::redirect('/', '/home');
 
-/* （ログインページ/ログインアクション）では、middlewareのguestを使用し、認証済みの場合にそのページへは遷移しないように制御している */
+
+/*
+（ログインページ/ログインアクション）では、middlewareのguestを使用し、認証済みの場合にそのページへは遷移しないように制御している
+ */
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.', 'middleware' => 'guest:admin'], function () {
     Route::get('login', 'LoginController@showLoginForm')->name('login');
     Route::post('login', 'LoginController@login')->name('login');
 });
 
-/* adminログイン済みのユーザーのlogout、/admin/homeへの遷移ではmiddlewareのAuthで認証をかけている */
+/*
+adminログイン済みのユーザーのlogout、/admin/homeへの遷移ではmiddlewareのAuthで認証をかけている
+*/
 Route::prefix('admin')->namespace('Admin')->as('admin.')->middleware(['auth:admin'])->group(function(){
     Route::post('logout', 'LoginController@logout')->name('logout');
     Route::get('home', 'HomeController@index')->name('home');
 });
 
-/* adminログイン後のリダイレクト */
+/*
+adminログイン後のリダイレクト
+*/
 Route::redirect('/admin', '/admin/home');
 
 Route::prefix('admin')->namespace('Admin')->as('admin.')->middleware(['auth:admin'])->group(function(){
